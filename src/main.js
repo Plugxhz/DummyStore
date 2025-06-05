1a5051c0a23bb65db9eb3af71940c6bd98b8146a
Enzo Sakatauskas <enzosakatauskas@gmail.com>
2025-06-05 20:36:27 -0300

    melhoria no main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-// src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { createPinia } from 'pinia';
 
 const app = createApp(App);
-app.use(createPinia());
 app.use(router);
 app.mount('#app');
