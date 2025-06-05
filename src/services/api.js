f18bcbb36e01fae8b5308f8070bc1703576c7398
Enzo Sakatauskas <enzosakatauskas@gmail.com>
2025-06-05 20:37:08 -0300

    melhoria no api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,8 @@
 /**
- * Serviço de API para fazer requisições para a API do Dummyjson.com
- *
- * Aqui estão as funções para:
  *  - getCategories: retorna as categorias de produtos
  *  - getProductsByCategory: retorna os produtos de uma categoria
  *  - getProductById: retorna um produto por id
  *  - getAllProducts: retorna todos os produtos, com opção de limitar e pular produtos
- *
- * Essas funções são utilizadas nos componentes para fazer requisições à API
- * e popular as páginas com os dados retornados
  */
 
 import axios from 'axios';
